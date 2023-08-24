d45c6dc094b8e5ca12d87888570a26b731e1182f
boner.engineer <boner.engineer@outlook.jp>
2023-08-24 11:07:16 +0900

    Storybook Add

--- src/stories/Pages/Page/index.tsx
+++ src/stories/Pages/Page/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Header } from './Header';
-import './page.css';
+import { Header } from '../../Molecules/Header';
+import './style.scss';
 
 type User = {
   name: string;
