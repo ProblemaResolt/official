e1a90bd163c50946705e0c8b329a1008ce3f5659
xxxx <boner.engineer@gmail.com>
2025-05-11 18:13:38 +0900

    スクロールバーレイアウト崩れ修正

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import '@unocss/preset-mini';
-import App from './App.jsx';
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 );
--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,7 @@ export default defineConfig({
     outDir: 'docs',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        404: resolve(__dirname, 'public/404.html')
+        main: resolve(__dirname, 'index.html')
       }
     }
   }
