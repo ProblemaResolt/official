f939c5032b365073cdc3dbdf1e016e89bed49fe5
xxxx <boner.engineer@gmail.com>
2025-05-11 18:19:19 +0900

    BrowserRouter tuika

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/official">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
